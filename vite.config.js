4d89150bd02c4df1a2ff846fb89ed67e33a363cd
kiran76 <ksknew76@gmail.com>
2024-07-12 17:55:16 +0530

    changed directory

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
+      external: [],
       output: {
         globals: {}
       }
     }
   }
 })
+  
