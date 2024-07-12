9cbd270e0f5e0af6bb6802080eafdebc89b3ecea
kiran76 <ksknew76@gmail.com>
2024-07-12 15:21:05 +0530

    vite config update

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react-icons', 'react-icons/hi'],
       output: {
         globals: {}
       }
